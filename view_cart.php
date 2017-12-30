9dbfd33775362159248eedf40e02ad302107a63d
iMac <svelasco5@student.rccd.edu>
2017-12-29 16:54:59 -0800

    correct spelling eror in loadCart.php

--- view_cart.php
+++ view_cart.php
@@ -16,7 +16,7 @@ if (!isset($_SESSION['u_id'])) {
     echo "<h1 style='text-align:center;margin-top:2em;'>Shopping Cart currently empty</h1>";
   } else { //user has added products to the sessions 'cart'
     echo "<h1 style='text-align:center;margin-top:2em;margin-bottom:2em;'>Shopping Cart</h1>";
-    include_once "./include/loadcart.php";
+    include_once "./include/loadCart.php";
   }
 }
  ?>
